0baa408eb4b8efdcf719a7ad0416d7e587cc2d8a
joztoz <jozef@ziak.me>
2025-08-04 03:50:56 +0000

    Aug 4, 2025, 5:50 AM

--- cypress/integration/04_klikanie_pisanie_interakcia/zaciatok.js
+++ cypress/integration/04_klikanie_pisanie_interakcia/zaciatok.js
@@ -2,18 +2,40 @@
 
 it('vytvorenie nového boardu', () => {
 
+    cy
+    .visit('/')
+
+cy
+    .get('#new-board')
+    .click()
+
+cy 
+    .get('#new-board input')
+    .type('My new board {enter}')
 
   
 });
 
 it('označenie boardu hviezdičkou', () => {
 
+    cy
+        .visit('/')
+    
+    cy
+        .get('.board_title')
+        .get('.Star')
+        .click({force:true, multiple: true})
+    
   
 
 });
 
-it('označenie tasku', () => {
+it.only('označenie tasku', () => {
 
+    cy
+        .visit('/board/34096040370')
   
-
+    cy
+        .get('.Task input')
+        .check(false)
 });
